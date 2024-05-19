e9dcb1989c1ec69a18f134cb8f52b8617be276fd
HUIJAEKO <xrhgmlwox@naver.com>
2024-05-19 16:08:33 +0900

    snail

---
+++ algorithm_study/src/Math/Snail.java
@@ -0,0 +1,22 @@
+package Math;
+
+import java.util.Scanner;
+
+public class Snail {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        long up = input.nextLong();
+        long down = input.nextLong();
+        long height = input.nextLong();
+
+        if (up >= height) {
+            System.out.println(1);
+            return;
+        }
+
+        long move = up - down;
+        long days = (height - down - 1) / move;
+
+        System.out.println(days+1);
+    }
+}

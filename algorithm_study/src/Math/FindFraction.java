c9e6c7c2fc2ff1e1fcd3ecbbaac39ec2b47ed8b7
HUIJAEKO <xrhgmlwox@naver.com>
2024-06-25 15:29:35 +0900

    math

---
+++ algorithm_study/src/Math/FindFraction.java
@@ -0,0 +1,30 @@
+package Math;
+
+import java.util.Scanner;
+
+public class FindFraction {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+        int sum = 0;
+        int n = 0;
+
+        while(sum<num){
+            n++;
+            sum += n;
+        }
+
+        int position = num - (sum - n);
+        int numerator, denominator;
+
+        if (n % 2 == 0) {
+            numerator = position;
+            denominator = (n + 1) - position;
+        } else {
+            numerator = (n + 1) - position;
+            denominator = position;
+        }
+
+        System.out.println(numerator + "/" + denominator);
+    }
+}

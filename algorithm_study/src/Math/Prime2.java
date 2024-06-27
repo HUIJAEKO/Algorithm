ade6c537af7e76371e7ae562f86d9587dd299874
HUIJAEKO <xrhgmlwox@naver.com>
2024-06-27 18:34:16 +0900

    math

--- algorithm_study/src/Math/Prime2.java
+++ algorithm_study/src/Math/Prime2.java
@@ -1,4 +1,44 @@
 package Math;
 
+import java.util.Scanner;
+
 public class Prime2 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int m = input.nextInt();
+        int n = input.nextInt();
+
+        int total = 0;
+        int min = n;
+
+        for(int i = m; i <= n; i++){
+            boolean isPrime = true;
+
+            if (i == 1) {
+                isPrime = false;
+            } else {
+                for(int j = 2; j <= i-1; j++){
+                    if(i % j == 0){
+                        isPrime = false;
+                        break;
+                    }
+                }
+            }
+
+            if(isPrime){
+                total += i;
+                if(i < min){
+                    min = i;
+                }
+            }
+        }
+
+        if(total == 0){
+            System.out.println(-1);
+        } else {
+            System.out.println(total);
+            System.out.println(min);
+        }
+
+    }
 }
--- algorithm_study/src/Math/PrimeFactorization.java
+++ algorithm_study/src/Math/PrimeFactorization.java
@@ -1,4 +1,20 @@
 package Math;
 
+import java.util.Scanner;
+
 public class PrimeFactorization {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        int num = input.nextInt();
+        while(num != 1){
+            for(int i=2; i<=num; i++){
+                if(num%i == 0){
+                    System.out.println(i);
+                    num /= i;
+                    break;
+                }
+            }
+        }
+    }
 }

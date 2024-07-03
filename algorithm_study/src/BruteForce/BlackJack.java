baa56b4e08257b8fb35afaed720afacbc1c25f65
HUIJAEKO <xrhgmlwox@naver.com>
2024-07-03 16:50:53 +0900

    brute force

---
+++ algorithm_study/src/BruteForce/BlackJack.java
@@ -0,0 +1,29 @@
+package BruteForce;
+
+import java.util.Scanner;
+
+public class BlackJack {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int N = input.nextInt();
+        int M = input.nextInt();
+        int sum = 0;
+
+        int[] num = new int[N];
+        for(int i=0; i<N; i++){
+            num[i] = input.nextInt();
+        }
+
+        for(int i=0; i<N-2; i++){
+            for(int j=i+1; j<N-1; j++){
+                for(int k=j+1; k<N; k++){
+                    int result = num[i] + num[j] + num[k];
+                    if(result <= M && result > sum){
+                        sum = result;
+                    }
+                }
+            }
+        }
+        System.out.println(sum);
+    }
+}
---
+++ algorithm_study/src/BruteForce/DivideSum.java
@@ -0,0 +1,4 @@
+package BruteForce;
+
+public class DivideSum {
+}

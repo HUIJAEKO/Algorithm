0eacc0b17bf196a0381ce39d4c30f441e633c831
HUIJAEKO <xrhgmlwox@naver.com>
2024-07-03 17:25:08 +0900

    divide

--- algorithm_study/src/BruteForce/DivideSum.java
+++ algorithm_study/src/BruteForce/DivideSum.java
@@ -1,4 +1,28 @@
 package BruteForce;
 
+import java.util.Scanner;
+
 public class DivideSum {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int N = input.nextInt();
+        int result = 0;
+
+        for(int i=1; i<N; i++){
+            int sum = i;
+            int number = i;
+
+            while(number != 0){
+                sum += number % 10;
+                number /= 10;
+            }
+
+            if(sum == N){
+                result = i;
+                break;
+            }
+        }
+
+        System.out.println(result);
+    }
 }

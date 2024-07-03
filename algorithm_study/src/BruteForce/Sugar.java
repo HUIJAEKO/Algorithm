c2812e4cbdeda1c114f2623d22a9b00bc16ca3fc
HUIJAEKO <xrhgmlwox@naver.com>
2024-07-03 19:15:47 +0900

    sugar

---
+++ algorithm_study/src/BruteForce/Sugar.java
@@ -0,0 +1,20 @@
+package BruteForce;
+
+import java.util.Scanner;
+
+public class Sugar {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int N = input.nextInt();
+        int box = -1;
+
+        for(int i=0; i<=N/5; i++){
+            int reminder = N - 5 * i;
+            if(reminder % 3 == 0){
+                int j = reminder / 3;
+                box = i + j;
+            }
+        }
+        System.out.println(box);
+    }
+}
